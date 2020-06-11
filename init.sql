c95fb882b393c03a03c7a0041e19ba1742fa977b
Fabian <fabianfromwald@gmx.at>
2020-06-11 11:44:30 +0200

    Update database design

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS HE;
---DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS File;
 DROP TABLE IF EXISTS HELink;
 
 
@@ -7,25 +7,25 @@ create table HELink (
     HELinkUuid uuid primary key
 );
 
-/*create table Student (
+create table File (
     id serial primary key,
-    fname text,
-    lname text
-);*/
+    Text text,
+    status text
+);
 
 create table HE (
 	HELinkUuid uuid references HELink,
 	HeUuid uuid,
 	fname text,
     lname text,
-	file text,
+	file serial REFERENCES File,
 	status text,
     primary key (HELinkUuid, HeUuid)
 );
 
 
 -- inserts
-
+/*
 insert into HELink (HELinkUuid) values ('cfc7103d-84e8-4190-898c-47b61f50296f');
 insert into HELink (HELinkUuid) values ('8f426c72-88ab-4efc-866d-2f947dd6c1ad');
 
@@ -36,13 +36,4 @@ insert into HE (HELinkUuid, HeUuid, fname, lname, status) values ('cfc7103d-84e8
 insert into HE (HELinkUuid, HeUuid, fname, lname, status) values ('8f426c72-88ab-4efc-866d-2f947dd6c1ad', '387977ad-ca6d-4df6-aaf2-ab4a357d6b83', 'Hansi', 'Hansman', 'Corrected');
 insert into HE (HELinkUuid, HeUuid, fname, lname, status) values ('8f426c72-88ab-4efc-866d-2f947dd6c1ad', 'b050065d-6056-425c-93ef-37b22908254c', 'Hansi', 'Hansman', 'Corrected');
 
-/*insert into Student (id, fname, lname) values (1, 'Hansi', 'Hansman');
-insert into Student (id, fname, lname) values (2, 'Heinrich', 'Meier');
-insert into Student (id, fname, lname) values (3, 'Axel', 'Schweiß');
-
-insert into HE (HELinkUuid, HeUuid, Student, status) values ('cfc7103d-84e8-4190-898c-47b61f50296f', '891c0d40-de5e-4cae-bc6b-24a98e059502', 1, 'Submitted');
-insert into HE (HELinkUuid, HeUuid, Student, status) values ('cfc7103d-84e8-4190-898c-47b61f50296f', '97c1629e-7053-4a58-89c2-7221dbf0a7ff', 2, 'Submitted');
-insert into HE (HELinkUuid, HeUuid, Student, status) values ('cfc7103d-84e8-4190-898c-47b61f50296f', '78fb1632-a42b-49a0-a371-95238a6e0761', 3, 'Submitted');
-
-insert into HE (HELinkUuid, HeUuid, Student, status) values ('8f426c72-88ab-4efc-866d-2f947dd6c1ad', '387977ad-ca6d-4df6-aaf2-ab4a357d6b83', 1, 'Corrected');
-insert into HE (HELinkUuid, HeUuid, Student, status) values ('8f426c72-88ab-4efc-866d-2f947dd6c1ad', 'b050065d-6056-425c-93ef-37b22908254c', 1, 'Corrected');*/
+*/
